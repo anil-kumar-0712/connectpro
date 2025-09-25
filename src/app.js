ad3e3e93cc22bc643d62e06525bedcc447f487bc
Anil Kumar <anilrongali323@gmail.com>
2025-09-25 21:17:55 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,43 @@
 const express=require('express');
 const app=express();
 
-app.use('/test,',(req,res)=>{
-    res.send("hello world");
+app.use("/admin",(req,res,next)=>{
+    const mytoken="xyz";
+    const comptoken=mytoken==="xyz"
+    if(!comptoken){
+        res.status(401).send("nope")
+    }
+     else{
+        next();
+     }   
+        
+});
+
+app.get("/admin/dashboard",(req,res)=>{
+    res.send("your dashboard is here")
+})
+app.get("/admin/delete",(req,res)=>{
+    res.send("youg have  permission to delete the data")
+});
+
+
+app.use("/user",(req,res,next)=>{
+    console.log("hello user")
+    const token="abc"
+    const token1=token="abc"
+    if(!token1){
+        res.status(401).send("the user is not valid")
+    }
+    else{
+        next();
+    }
 })
 
-app.use('/,test',(req,res)=>{
-    res.send("hello hello")
-})
-
-app.use('/contact/hii',(req,res)=>{
-    res.send("hoii hiii")
-})
-
-app.use("/",(req,res)=>{
-    res.send("hello anil")
+app.get("/user1",(req,res)=>{
+    res.send("hello anil");
 })
 
 
-app.listen(7777,()=>{
+app.listen(8888,()=>{
     console.log("your server is running")
 });
